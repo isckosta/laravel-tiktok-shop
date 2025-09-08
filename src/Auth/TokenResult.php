970197762c1588e1c446c40d60c6380455767300
Israel Costa <isckosta@live.com>
2025-09-08 20:05:22 -0300

    feat: extend TokenResult with shop information, scopes and seller details

--- src/Auth/TokenResult.php
+++ src/Auth/TokenResult.php
@@ -2,37 +2,59 @@
 
 namespace TikTokShop\Auth;
 
+use Carbon\Carbon;
+
 class TokenResult
 {
     public string $accessToken;
     public string $refreshToken;
-    public \Carbon\Carbon $accessTokenExpiresAt;
-    public ?\Carbon\Carbon $refreshTokenExpiresAt;
+    public Carbon $accessTokenExpiresAt;
+    public ?Carbon $refreshTokenExpiresAt;
     public ?string $openId;
+    public array $scopes;
+    public ?string $shopCipher;
+    public ?string $shopCode;
+    public ?string $shopId;
+    public ?string $shopName;
+    public ?string $shopRegion;
+    public ?string $shopSellerType;
     public ?string $sellerName;
     public ?string $sellerRegion;
     public ?int $userType;
 
     public function __construct(
-        string $accessToken,
-        string $refreshToken,
-        \Carbon\Carbon $accessTokenExpiresAt,
-        ?\Carbon\Carbon $refreshTokenExpiresAt = null,
+        string  $accessToken,
+        string  $refreshToken,
+        Carbon  $accessTokenExpiresAt,
+        ?Carbon $refreshTokenExpiresAt = null,
         ?string $openId = null,
-        public ?string $shopCipher = null,
-        public array $scopes = [],
+        array   $scopes = [],
+        ?string $shopCipher = null,
+        ?string $shopCode = null,
+        ?string $shopId = null,
+        ?string $shopName = null,
+        ?string $shopRegion = null,
+        ?string $shopSellerType = null,
         ?string $sellerName = null,
         ?string $sellerRegion = null,
-        ?int $userType = null,
+        ?int    $userType = null,
     ) {
-        $this->accessToken = $accessToken;
-        $this->refreshToken = $refreshToken;
-        $this->accessTokenExpiresAt = $accessTokenExpiresAt;
+        $this->accessToken           = $accessToken;
+        $this->refreshToken          = $refreshToken;
+        $this->accessTokenExpiresAt  = $accessTokenExpiresAt;
         $this->refreshTokenExpiresAt = $refreshTokenExpiresAt;
-        $this->openId = $openId;
-        $this->sellerName = $sellerName;
-        $this->sellerRegion = $sellerRegion;
-        $this->userType = $userType;
+        $this->openId                = $openId;
+        $this->scopes                = $scopes;
+        $this->shopCipher            = $shopCipher;
+        $this->shopCode              = $shopCode;
+        $this->shopId                = $shopId;
+        $this->shopName              = $shopName;
+        $this->shopRegion            = $shopRegion;
+        $this->shopSellerType        = $shopSellerType;
+        $this->sellerName            = $sellerName;
+        $this->sellerRegion          = $sellerRegion;
+        $this->userType              = $userType;
     }
 }
+
 
